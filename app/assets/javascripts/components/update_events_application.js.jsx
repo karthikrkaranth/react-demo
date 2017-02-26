9967baaf8d79621ceed176bf7497e1a1f00f1481
karthik <karthikrkaranth@gmail.com>
2017-02-26 13:36:19 +0000

    react gem demo

---
+++ app/assets/javascripts/components/update_events_application.js.jsx
@@ -0,0 +1,25 @@
+var UpdateEvent = React.createClass({
+handleSearch : function (events) {
+    this.setState({ events: events });
+  },
+render: function() {
+  return(
+    <div className="container">
+      <div className="jumbotron">
+        <h1>ReactJS Tutorial</h1>
+        <p>by Piotr Jaworski</p>
+      </div>
+      <div className="row">
+        <div className="col-md-4">
+          <SearchForm handleSearch={this.handleSearch} />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-12">
+          <EventTable events={this.state.events} />
+        </div>
+      </div>
+    </div>
+  )
+}
+});
